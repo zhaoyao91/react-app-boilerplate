e7066472248ad66157b3d6f77c8b961934375c7c
zhaoyao91 <zhaoyao91@gmail.com>
2019-03-27 00:56:38 +0800

    add useLocales

--- src/App/AboutPage/OtherPage/index.js
+++ src/App/AboutPage/OtherPage/index.js
@@ -1,13 +1,31 @@
-import React from "react";
-import { Translation } from "react-i18next";
+import React, { useState } from "react";
 
-const OtherPage = () => (
-  <>
-    <h1>About Other</h1>
-    <p>
-      <Translation>{t => t("app:title")}</Translation>
-    </p>
-  </>
-);
+import useLocales from "../../hooks/useLocales";
+
+const locales = {
+  namespace: __filename,
+  en: {
+    name: "Bob"
+  },
+  zh: {
+    name: "鲍勃"
+  }
+};
+
+const OtherPage = () => {
+  const { Translation } = useLocales(locales);
+
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>About Other</h1>
+      <p>
+        <Translation>{t => t("name")}</Translation>
+      </p>
+      <p onClick={() => setCount(count + 1)}>{count}</p>
+    </>
+  );
+};
 
 export default OtherPage;
