c98e0f17d8a59e1473ddf6376a70d5916814106e
zhaoyao91 <zhaoyao91@gmail.com>
2019-03-27 01:47:24 +0800

    add apis to Calculator page

---
+++ src/App/CalculatorPage/apis/getStockInfo.js
@@ -0,0 +1,23 @@
+// @flow
+
+import axios from "axios";
+import urlJoin from "url-join";
+
+import { apiServerBaseUrl } from "../../../configs";
+
+function buildUrl(code) {
+  return urlJoin(apiServerBaseUrl, "/code/", code);
+}
+
+type StockInfo = {
+  code: string,
+  name: string
+};
+
+async function getStockInfo(code: string): Promise<StockInfo> {
+  const url = buildUrl(code);
+  const response = await axios.get(url);
+  return response.data;
+}
+
+export default getStockInfo;
