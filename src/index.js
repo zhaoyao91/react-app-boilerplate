25c0b70ff14b6bf298928621516a2e10da44fdee
zhaoyao91 <zhaoyao91@gmail.com>
2019-03-26 21:52:24 +0800

    init app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+// load global style
+import "antd-mobile/dist/antd-mobile.css";
+
+// mount app
+ReactDOM.render(<App />, document.getElementById("root"));
